352668edb458795e3d4299818b2b61c68620322a
chaitu1 <chaitu143@gmail.com>
2024-05-01 14:13:27 +0530

    push

--- .github/actions/setup-java/java.sh
+++ .github/actions/setup-java/java.sh
@@ -8,23 +8,23 @@ apt-get  update && apt-get install curl unzip -y
 
 # apt-get install -y nodejs
 
-# apt-get install openjdk-${version}-jdk -y
+ apt-get install openjdk-${version}-jdk -y
 
 # apt-get install maven -y
 
-curl -L -o sonar.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.8.1.3023-linux.zip
+#curl -L -o sonar.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.8.1.3023-linux.zip
 
-unzip -d /opt/ sonar.zip 
+#unzip -d /opt/ sonar.zip 
 
 #ln -s /opt/sonar-scanner-4.8.1.3023-linux/bin/sonar-scanner /bin/sonar-scanner
 
-echo "export PATH=/opt/sonar-scanner-4.8.1.3023-linux/bin:\${PATH}" > /etc/profile.d/maven.sh
+#echo "export PATH=/opt/sonar-scanner-4.8.1.3023-linux/bin:\${PATH}" > /etc/profile.d/maven.sh
 
-source /etc/profile.d/maven.sh
+#source /etc/profile.d/maven.sh
 
 # export PATH="/opt/sonar-scanner-4.8.1.3023-linux/bin:${PATH}"
 
-sonar-scanner --version
+#sonar-scanner --version
 
 
 
