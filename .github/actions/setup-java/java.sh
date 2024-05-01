a7e811cbc3f2b424635b4da54f591af73f949c37
chaitu1 <chaitu143@gmail.com>
2024-05-01 13:52:16 +0530

    push

--- .github/actions/setup-java/java.sh
+++ .github/actions/setup-java/java.sh
@@ -4,19 +4,21 @@ version=$1
 
 apt-get  update && apt-get install curl unzip -y
 
-curl -sL https://deb.nodesource.com/setup_20.x | bash
+# curl -sL https://deb.nodesource.com/setup_20.x | bash
 
-apt-get install -y nodejs
+# apt-get install -y nodejs
 
-apt-get install openjdk-${version}-jdk -y
+# apt-get install openjdk-${version}-jdk -y
 
-apt-get install maven -y
+# apt-get install maven -y
 
 curl -L -o sonar.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.8.1.3023-linux.zip
 
-unzip -d /opt/ sonar.zip
+unzip -d /opt/ sonar.zip 
 
-export PATH="/opt/sonar-scanner-4.8.1.3023-linux/bin:${PATH}"
+ln -s /opt/sonar-scanner-4.8.1.3023-linux/bin/sonar-scanner /bin/sonar-scanner
+
+# export PATH="/opt/sonar-scanner-4.8.1.3023-linux/bin:${PATH}"
 
 sonar-scanner --version
 
